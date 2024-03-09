a3b1628bca96625749d60a6d27f6662fe95ff76c
malak_bendalla <malak.bendalla22@gmail.com>
2024-03-09 20:40:12 +0100

    create WebSocketEventListener

---
+++ src/main/java/com/malak/chatdemo/config/WebSocketEventListener.java
@@ -0,0 +1,38 @@
+package com.malak.chatdemo.config;
+
+import com.malak.chatdemo.chat.ChatMessage;
+import com.malak.chatdemo.chat.MessageType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(
+            SessionDisconnectEvent event
+    ) {
+        // TO DO
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("user disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            // to inform other that x lived the chat
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+
+}
